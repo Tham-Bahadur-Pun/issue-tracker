7e48374c611059d652763d34e89347ed32aade53
Tham Bahadur Pun <thambaahdurpun123@gmail.com>
2024-04-08 13:48:35 +0545

    Radix Ui configured

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Button } from "@radix-ui/themes";
 
 const IssuePage = () => {
   return (
-    <div>IssuePage</div>
-  )
-}
+    <>
+      <div>IssuePage</div>
+      <Button>New Issue</Button>
+    </>
+  );
+};
 
-export default IssuePage
+export default IssuePage;
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,6 @@ export default function Home() {
   return (
   <main>
     <h1>hello world</h1>
-    </main>
+  </main>
   );
 }
