800bd575d444b2524c1912e8759f344e7be61cd8
Tham Bahadur Pun <thambaahdurpun123@gmail.com>
2024-04-13 20:58:35 +0545

    disabled ssr

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,54 +1,61 @@
 "use client";
 import { Button, Callout, Text, TextField } from "@radix-ui/themes";
 import React, { useState } from "react";
-import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { Controller, useForm } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchemas";
-import {z} from "zod"
+import { z } from "zod";
 import Spinner from "@/app/components/Spinner";
+import dynamic from "next/dynamic";
 
-type IssueForm = z.infer<typeof createIssueSchema>
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
+
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
-  const { register, control, handleSubmit, formState: {errors} } = useForm<IssueForm>({
-    resolver: zodResolver(createIssueSchema)
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
   });
-  const router = useRouter()
+  const router = useRouter();
 
-  const [error, setError] = useState<string>('')
-  const [isSubmitting, setIsSubmitting]=useState<boolean>(false)
+  const [error, setError] = useState<string>("");
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
 
-  const onSubmit =  handleSubmit (async (data) => {
+  const onSubmit = handleSubmit(async (data) => {
     try {
-      setIsSubmitting(true)
-      await axios.post('/api/issues',data)
-      router.push('/issues')
-    } catch (error){
-      setIsSubmitting(false)
-      setError('An unexpected error occured.')
-    }})
+      setIsSubmitting(true);
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setIsSubmitting(false);
+      setError("An unexpected error occured.");
+    }
+  });
 
   return (
     <div className="max-w-xl p-5">
-      {
-        error && (
-          <Callout.Root className="mb-5" color="red">
-            <Callout.Text>
-              {error}
-            </Callout.Text>
-          </Callout.Root>
-        )
-      }
-      <form
-        className="space-y-3"
-        onSubmit={onSubmit}
-      >
+      {error && (
+        <Callout.Root className="mb-5" color="red">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form className="space-y-3" onSubmit={onSubmit}>
         <TextField.Root placeholder="Title" {...register("title")} />
-        {errors.title && <Text as="p" color="red">{errors.title.message}</Text>}
+        {errors.title && (
+          <Text as="p" color="red">
+            {errors.title.message}
+          </Text>
+        )}
 
         <Controller
           name="description"
@@ -57,8 +64,14 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
-        {errors.description && <Text as="p" color="red">{errors.description.message}</Text>}
-        <Button disabled={isSubmitting}>Create New Issue {isSubmitting && <Spinner/>}</Button>
+        {errors.description && (
+          <Text as="p" color="red">
+            {errors.description.message}
+          </Text>
+        )}
+        <Button disabled={isSubmitting}>
+          Create New Issue {isSubmitting && <Spinner />}
+        </Button>
       </form>
     </div>
   );
