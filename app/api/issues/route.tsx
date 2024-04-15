2a60389b8d0e92f6760af26d3c860cfc63f9ee4a
Tham Bahadur Pun <thambaahdurpun123@gmail.com>
2024-04-15 16:45:17 +0545

    fix-caching-issue

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -1,17 +1,20 @@
-import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
 import { issueSchema } from "../../validationSchemas";
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = issueSchema.safeParse(body);
+  
   if (!validation.success)
-    return NextResponse.json(validation.error.errors, { status: 400 });
+    return NextResponse.json(validation.error.format(), { status: 400 });
+
   const newIssue = await prisma.issue.create({
     data: {
       title: body.title,
       description: body.description,
     },
   });
+
   return NextResponse.json(newIssue, { status: 201 });
 }
